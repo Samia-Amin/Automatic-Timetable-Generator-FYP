24f39c999ec6afb78d561b3c5967178c526caa3d
AzkaAbubakar <61390773+AzkaAbubakar@users.noreply.github.com>
2020-02-24 01:07:32 +0500

    Header, Footer Added
    Header and Footer for the website.

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Automatic Timetable Generator</title>
+	<!-- Meta Tags -->
+	<meta charset="UTF-8">
+	<meta name="description" content="Automatic Timetable Generator ">
+	<meta name="keywords" content="event, Automatic Timetable Generator, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i" rel="stylesheet">
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="assets/css/animate.css"/>
+	<link rel="stylesheet" href="assets/css/style.css"/>
+</head>
+
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+	<!-- header section -->
+	<header class="header-section">
+		<div class="container">
+			<!-- logo -->		
+			<div class="header-info py-3">
+				<h1 style="text-align:center">Automatic Timetable Generator</h1>
+			</div>
+		</div>
+	</header>
+	<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #020031;">
+		<div class="container">
+			<ul class="navbar-nav">
+				<li class="nav-item"><a class="nav-link" href="dashboard.php">Home</a></li>
+				<li class="nav-item"><a class="nav-link" href="faculty.php">Faculty</a></li>
+				<li class="nav-item"><a class="nav-link" href="semester.php">Semester</a></li>
+				<li class="nav-item"><a class="nav-link" href="subject.php">Subjects</a></li>
+				<li class="nav-item"><a class="nav-link" href="room.php">Room</a></li>
+				<li class="nav-item"><a class="nav-link" href="time.php">Timing</a></li>
+				<li class="nav-item"><a class="nav-link" href="algorithm.php">Algorithm</a></li>
+				<li class="nav-item pull-right"><a class="nav-link" href="logout.php">Logout</a></li>
+			</ul>
+		</div>
+	</nav>
+	<!-- Header section end -->
